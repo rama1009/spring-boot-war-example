2864f9e88e2c4a317389a128192f31f80533a944
rama1009 <ramakrishnajangati@gmail.com>
2025-01-27 13:39:07 +0530

    Update HelloWorldApplication.javaoi

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "chalu babu chalu repu chuskundam le";
+        return "Agam evala code cheyalsindhe.";
     }
 
 }
